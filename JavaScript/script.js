52390921f3e4adfb8ffd12c1e3068e6e9b453de0
MukitHossen7 <hossenmukit7@gmail.com>
2025-03-17 12:48:31 +0600

    refactor: add voting eligibility check and log message for clarity

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -179,3 +179,10 @@ const numbers = [1, 4, 5, 5, 6, 7, 6, 8, 9, 10, 4];
 //   }
 // }
 // findDuplicate(numbers);
+
+let x = 18;
+if (x > 18) {
+  console.log("You are eligible for vote");
+} else {
+  console.log("You are not eligible for vote");
+}

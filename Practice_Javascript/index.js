e8d7bb21fdc4bbffe4a0864ec0edeecf48ed6946
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-23 19:49:28 +0600

    feat: update increment function to accept a parameter for dynamic counting

--- Practice_Javascript/index.js
+++ Practice_Javascript/index.js
@@ -1,6 +1,12 @@
 const counter = {
   count: 0,
-  increment: function () {
-    console.log(this.count++);
+  increment: function (newCount) {
+    console.log((this.count += newCount));
   },
 };
+
+counter.increment(1);
+counter.increment(2);
+counter.increment(3);
+counter.increment(4);
+counter.increment(5);

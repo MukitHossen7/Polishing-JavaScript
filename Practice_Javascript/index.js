abc9e893948867df9be112808c07b321187b9469
MukitHossen7 <hossenmukit7@gmail.com>
2025-06-23 19:52:17 +0600

    refactor: replace object-based counter with a functional approach

--- Practice_Javascript/index.js
+++ Practice_Javascript/index.js
@@ -1,12 +1,22 @@
-const counter = {
-  count: 0,
-  increment: function (newCount) {
-    console.log((this.count += newCount));
-  },
-};
+// const counter = {
+//   count: 0,
+//   increment: function (newCount) {
+//     console.log((this.count += newCount));
+//   },
+// };
 
-counter.increment(1);
-counter.increment(2);
-counter.increment(3);
-counter.increment(4);
-counter.increment(5);
+// counter.increment(1);
+// counter.increment(2);
+// counter.increment(3);
+// counter.increment(4);
+// counter.increment(5);
+
+const counter = (newCount) => {
+  let count = 0;
+  return count + newCount;
+};
+console.log(counter(1));
+console.log(counter(2));
+console.log(counter(3));
+console.log(counter(4));
+console.log(counter(5));
